29201dc7985a94e29944d807547f86ac36096a41
Shane Leonard <shanel@stanford.edu>
2017-04-11 01:15:41 -0700

    Basic scaffolding for the project, including ops transplant

---
+++ oz/__init__.py
@@ -0,0 +1,2 @@
+def oz():
+    return "We're off to see the wizard!"
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='oz-cli',
+      version='0.1',
+      description='Swiftly execute complex commands in the shell',
+      url='https://github.com/shaneleonard/oz',
+      author='Shane Leonard',
+      author_email='shane.william.leonard@gmail.com',
+      license='MIT',
+      packages=['oz'],
+      scripts=['bin/oz'],
+      zip_safe=False)
